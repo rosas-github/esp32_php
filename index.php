6079cf7d9504cedf3b2c37cdb2dc2e331392574a
rosas-github <61634031+rosas-github@users.noreply.github.com>
2020-02-29 20:40:26 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,25 @@
 <?php
-    // esp32/pepito.php?nombre=Robert pepe
-    
+
 $servername = "ec2-35-172-85-250.compute-1.amazonaws.com";
 $database = "d7pmm1dodikbv5";
 $username = "ejxfckxmrixihs";
 $password = "6e1eaa5b5aa1d10c3290bb1795f9263bb629a3bb80ffda285c33862603a95273";
 
-$nombre = $_GET['nombre'];
-
+$espid = $_GET["espid"]; //igual a nombre en consulta "select"
+$cmd = $_GET["cmd"];
 
 // Conectando y seleccionado la base de datos  
 $dbconn = pg_connect("host=" . $servername . " dbname=" . $database . " user=". $username . " password=" . $password)
-    or die('No se ha podido conectar: ' . pg_last_error());
+    or die('Error - No se ha podido conectar: ' . pg_last_error());
 
 // Realizando una consulta SQL
-$query = "SELECT * FROM espcommands where espid like '" . $nombre . "' " ;
-//$query = 'SELECT * FROM espcommands'
+//    INSERT INTO espcommands (espid, espcmd) VALUES ('Robert', 'LED_ON')
+$query = "INSERT INTO espcommands (espid, espcmd) VALUES ('" . $espid . "', '" . $cmd . "' ) " ;
+echo $query;
 //echo $query;
 
 $result = pg_query($query) or die('La consulta fallo: ' . pg_last_error());
-
-while($row = pg_fetch_array($result, null, PGSQL_ASSOC)) {
-         $myArray[] = $row;
-}
-echo json_encode($myArray);
+echo "Insert fue correcto";
 
 // Liberando el conjunto de resultados
 pg_free_result($result);
@@ -31,5 +27,4 @@ pg_free_result($result);
 // Cerrando la conexión
 pg_close($dbconn);
 
-//echo "<h2>Hola, desde Rosas-github</2>";
 ?>
