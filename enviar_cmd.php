0417c43d8f05b7422c49280811ea4491f0533740
rosas-github <61634031+rosas-github@users.noreply.github.com>
2020-02-29 21:04:37 -0300

    Update enviar_cmd.php

--- enviar_cmd.php
+++ enviar_cmd.php
@@ -1,12 +1,13 @@
 <?php
 
+
 $servername = "ec2-35-172-85-250.compute-1.amazonaws.com";
 $database = "d7pmm1dodikbv5";
 $username = "ejxfckxmrixihs";
 $password = "6e1eaa5b5aa1d10c3290bb1795f9263bb629a3bb80ffda285c33862603a95273";
 
-$espid = $_GET["espid"]; //igual a nombre en consulta "select"
-$cmd = $_GET["cmd"];
+$espid = $_POST["espid"]; //igual a nombre en consulta "select"
+$cmd = $_POST["cmd"]; //GET
 
 // Conectando y seleccionado la base de datos  
 $dbconn = pg_connect("host=" . $servername . " dbname=" . $database . " user=". $username . " password=" . $password)
@@ -16,7 +17,6 @@ $dbconn = pg_connect("host=" . $servername . " dbname=" . $database . " user=". 
 //    INSERT INTO espcommands (espid, espcmd) VALUES ('Robert', 'LED_ON')
 $query = "INSERT INTO espcommands (espid, espcmd) VALUES ('" . $espid . "', '" . $cmd . "' ) " ;
 echo $query;
-//echo $query;
 
 $result = pg_query($query) or die('La consulta fallo: ' . pg_last_error());
 echo "Insert fue correcto";
